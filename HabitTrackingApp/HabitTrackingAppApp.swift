17464f7838be9b2ecacc372435f15d48f71e00ab
Sean Dooley <s.dooley@ctidigital.com>
2024-11-20 15:03:47 +0000

    Initial Commit

---
+++ HabitTrackingApp/HabitTrackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingAppApp.swift
+//  HabitTrackingApp
+//
+//  Created by Sean Dooley on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
